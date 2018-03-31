a1e17f15f4f12a44803ab149953b3ecfeb649fa9
Alex Khischenko <Khischenko.A@gmail.com>
2018-03-31 19:31:56 +0300

    Lesson 9

--- src/index/index.js
+++ src/index/index.js
@@ -1,17 +1,19 @@
 import './index.scss';
 
-const name = prompt('');
+const name = prompt("Enter loggin");
 const secretName = 'admin';
 const pass = 123;
 
 
+
 if (name === secretName) {
     const password = prompt('Enter password');
-    if (password === pass) {
+    if (password == pass) {
         console.log("Hello, admin");
     } else {
         console.log("Password is wrong");
-        }
+    }
+
 } else {
-    console.log("Not autorised");
+    console.log("Loggin is wrong");
 }
